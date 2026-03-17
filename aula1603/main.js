214881bc77b8d057c514d5bb1073ecc6494d86fe
anajuliasspj-ui <anajuliasspj@gmail.com>
2026-03-17 07:43:49 -0300

    Initialize payment processing in main.js
    Import payment modules and process transactions.

---
+++ aula1603/main.js
@@ -0,0 +1,11 @@
+import {pagamentoPix} from './PagamentoPix.js';
+import {PagamentoBoleto} from './PagamentoBoleto.js';
+import {PagamentoCartao} from './PagamentoCartao.js';
+
+const contaLuz.processar();
+compraNotebook.processar();
+
+console.log("---RECIBOS---")
+console.log(contaLuz.exibirRecibo());
+console.log(compraNotebook.exibirRecibo());
+console.log(compraCafe.exibirRecibo());
